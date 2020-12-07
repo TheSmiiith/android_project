cfa730e50571bf693b5e723a659557ba996d5b0a
TheSmiiith <kovacs_attila99@outlook.hu>
2020-12-07 21:04:48 +0100

    view binding implemented

--- app/src/main/java/com/example/androidproject/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/androidproject/fragments/ProfileFragment.kt
@@ -6,15 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.androidproject.R
+import com.example.androidproject.databinding.ActivityMainBinding
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        /* View Binding */
+        _binding = ActivityMainBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
