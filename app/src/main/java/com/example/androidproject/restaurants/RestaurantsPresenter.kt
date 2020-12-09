9aa518cb41b2c9f3ebe8a867335fac89145c8120
TheSmiiith <kovacs_attila99@outlook.hu>
2020-12-09 21:03:21 +0100

    test request from API

--- app/src/main/java/com/example/androidproject/restaurants/RestaurantsPresenter.kt
+++ app/src/main/java/com/example/androidproject/restaurants/RestaurantsPresenter.kt
@@ -1,10 +1,35 @@
 package com.example.androidproject.restaurants
 
+import android.util.Log
 import com.example.androidproject.models.Restaurant
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import retrofit2.Retrofit
+import java.lang.Exception
+import kotlin.reflect.typeOf
 
 class RestaurantsPresenter(view: RestaurantsContract.View) : RestaurantsContract.Presenter(view) {
     override fun getRestaurants() {
-        val restaurants: ArrayList<Restaurant> = ArrayList()
-        view.setRestaurants(restaurants)
+        val client = OkHttpClient()
+        val request: Request = Request.Builder()
+            .url("https://opentable.herokuapp.com/api/restaurants?country=US&page=1")
+            .build()
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response: Response = client.newCall(request).execute()
+                if (!response.isSuccessful) {
+                    // TODO: implement onError() function to all views
+                }
+                Log.d("SUCCESS", response.message())
+            } catch (e: Exception) {
+                // TODO: implement onError() function to all views
+            }
+        }
     }
+
 }
